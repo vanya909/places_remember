a323c9cd624dfba6f05659af13b219ae04ea7063
Ivan <vanya909x75@gmail.com>
2022-02-24 20:10:54 +0700

    Add home page tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_reverse_lazy_status_code(self):
+        response = self.client.get(reverse_lazy('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse_lazy('home'))
+        self.assertTemplateUsed(response, 'pages/home.html')
